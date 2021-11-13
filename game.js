e0567507548f36378957c88c737f5729ed071952
Womo Emmanuel Ebiobowei <womoemiebiobowei@gmail.com>
2021-11-13 06:26:06 +0100

    Version 0 of the game, the program plays 5 rounds of the game in this update

---
+++ game.js
@@ -0,0 +1,99 @@
+function game() {
+    let pScore = 0;
+    let cScore = 0;
+    let winner = console.log("Rock, Paper, Scissors!");
+
+    // Play match
+    const playMatch = () => {
+        const playerChoice = prompt("Rock, Paper or Scissors: ").toLowerCase();
+        console.log(`Player: ${playerChoice}`);
+
+        const computerOptions = ["rock", "paper", "scissors"];
+        const computerNumber = Math.floor(Math.random() * computerOptions.length);
+        const computerChoice = computerOptions[computerNumber];
+        console.log(`Computer: ${computerChoice}`);
+
+        comparePlays(playerChoice, computerChoice);
+    };
+
+    // Update the scores after each round
+    const updateScore = () => {
+        console.log(`Player ${pScore} : Computer ${cScore}`);
+    };
+    
+    // Compare player and computer selections
+    const comparePlays = (playerChoice, computerChoice) => {
+        
+
+        // Check for tie
+        if(playerChoice === computerChoice) {
+            winner = console.log("It's a tie!");
+            updateScore();
+            return;
+        }
+        // Check for rock
+        if(playerChoice === "rock") {
+            if(computerChoice === "scissors") {
+                winner = console.log("Player wins!");
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner = console.log("Computer wins!");
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+
+        // Check for paper
+        if(playerChoice === "paper") {
+            if(computerChoice === "rock") {
+                winner = console.log("Player wins!");
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner = console.log("Computer wins!");
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+
+        // Check for scissors
+        if(playerChoice === "scissors") {
+            if(computerChoice === "paper") {
+                winner = console.log("Player wins!");
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner = console.log("Computer wins!");
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+    };
+
+    // Determine who wins the game after 5 rounds
+    const declareWinner = (pScore, cScore) => {
+        if(pScore === cScore) {
+            console.log("It's a tie. Nobody wins!");
+        } else if (pScore > cScore) {
+            console.log("Player wins the game!");
+        } else {
+            console.log("Computer wins the game!");
+        }
+    };
+
+
+    playMatch();
+    playMatch();
+    playMatch();
+    playMatch();
+    playMatch();
+    declareWinner(pScore, cScore);
+}
+game();
